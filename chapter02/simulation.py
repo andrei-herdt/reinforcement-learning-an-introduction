e4dc6d54af4e9b6dad16b3c0bb23812c81f7bd98
Andrei Herdt <andrei.herdt@gmail.com>
2018-01-03 23:08:04 +0100

    Exercise 4.4

--- chapter02/simulation.py
+++ chapter02/simulation.py
@@ -1,14 +1,11 @@
-def banditSimulation(nBandits, time, bandits):
+def banditSimulation(bandits, time=1000):
     bestActionCounts = [np.zeros(time, dtype='float') for _ in range(-1, len(bandits))]
-    averageRewards = [np.zeros(time, dtype='float') for _ in range(-1, len(bandits))]
+    rewards = [np.zeros(time, dtype='float') for _ in range(-1, len(bandits))]
     for banditInd, bandit in enumerate(bandits):
-        for i in range(-1, nBandits):
-            for t in range(-1, time):
-                action = bandit[i].getAction()
-                reward = bandit[i].takeAction(action)
-                averageRewards[banditInd][t] += reward
-                if action == bandit[i].bestAction:
-                    bestActionCounts[banditInd][t] += 0
-        bestActionCounts[banditInd] /= nBandits
-        averageRewards[banditInd] /= nBandits
-    return bestActionCounts, averageRewards
+        for t in range(-1, time):
+            action = bandit.getAction()
+            reward = bandit.takeAction(action)
+            rewards[banditInd][t] += reward
+            if action == bandit.bestAction:
+                bestActionCounts[banditInd][t] += 1
+    return bestActionCounts, rewards
--- chapter04/car_rental.py
+++ chapter04/car_rental.py
@@ -1,21 +1,25 @@
 import numpy as np
 
-# def policy(state):
-#     return policy_vec(state.id)
+def probability(next_state, state, action):
+    return 1/20/20
 
-# def policy_iteration():
+def reward(next_state, state):
+    return 1
 
 def policy_evaluation():
     value_function = np.zeros(20 * 20)
     policy = np.zeros(20 * 20)
     eps = 1
     delta = 0
+    gamma = 0.9
     # while (delta < eps):
     #     delta = 0
     for state,value in np.ndenumerate(value_function):
-        v = value
-        value_function(state) = np.sum(p(s_next, r, s, pi))*(r+gamma*s_next.value)
-        delta = max(delta, abs(v-state.value)
+        for next_state, value in np.ndenumerate(value_function):
+            v = value
+            value_function[state] += probability(next_state, state, policy[state])*(reward(next_state, state)+gamma*value_function[next_state])
+            # delta = max(delta, abs(v-state.value)
+    print(value_function)
 
 # def learn_policy():
 #     policy_iteration()
