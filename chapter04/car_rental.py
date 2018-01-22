90bbdaea7789d77ff78db9d225d6206fba829e82
Andrei Herdt <Andrei.Herdt@bmw.de>
2018-01-22 09:00:50 +0100

    Add policy evulation for one station

--- chapter04/car_rental.py
+++ chapter04/car_rental.py
@@ -17,22 +17,31 @@ def prob_rent_2(number_cars):
     llambda = 4
     return math.pow(llambda, number_cars)/math.factorial(number_cars)*math.exp(-llambda)
 
-def reward(next_state, state):
-    return 1
+def policy(state):
+    return 0
+
+def reward(cars_rented):
+    return 10*cars_rented
 
 def policy_evaluation():
     value_function = np.zeros(20 * 20)
-    policy = np.zeros(20 * 20)
     eps = 1
     delta = 0
     gamma = 0.9
     # while (delta < eps):
     #     delta = 0
-    for state,value in np.ndenumerate(value_function):
-        for next_state, value in np.ndenumerate(value_function):
-            v = value
-            value_function[state] += probability(next_state, state, policy[state])*(reward(next_state, state)+gamma*value_function[next_state])
-            # delta = max(delta, abs(v-state.value)
+    total_capacity = 20
+    for cars_available,value in np.ndenumerate(value_function):
+        state = cars_available
+        cars_moved = policy(state)
+        # for next_state, value in np.ndenumerate(value_function):
+            # v = value
+        for cars_rented in range(0,cars_available):
+            capacity = total_capacity - cars_available + cars_rented - cars_moved
+            for cars_returned in range(0,capacity):
+                next_state = cars_available + cars_rented + cars_returned
+                value_function[state] += prob_rent_1(cars_rented)*prob_return_1*(reward(cars_rented)+gamma*value_function(next_state))
+        # delta = max(delta, abs(v-state.value)
     print(value_function)
 
 # def learn_policy():
