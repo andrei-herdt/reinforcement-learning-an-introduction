869473b4735801dc88b618f2c8b8b25c67370f29
Herdt <andrei.herdt@bmw.de>
2018-01-23 09:28:40 +0100

    Debug and add plot

--- chapter04/car_rental.py
+++ chapter04/car_rental.py
@@ -1,5 +1,8 @@
 import numpy as np
 import math as math
+import matplotlib.pyplot as plt
+
+import pdb
 
 def prob_return_1(number_cars):
     llambda = 3
@@ -24,14 +27,15 @@ def reward(cars_rented):
     return 10*cars_rented
 
 def policy_evaluation():
-    value_function = np.zeros(20 * 20)
+    value_function = np.zeros(20)
     eps = 1
     delta = 0
     gamma = 0.9
     # while (delta < eps):
     #     delta = 0
     total_capacity = 20
-    for cars_available,value in np.ndenumerate(value_function):
+    for index,value in np.ndenumerate(value_function):
+        cars_available = index[0]
         state = cars_available
         cars_moved = policy(state)
         # for next_state, value in np.ndenumerate(value_function):
@@ -39,10 +43,20 @@ def policy_evaluation():
         for cars_rented in range(0,cars_available):
             capacity = total_capacity - cars_available + cars_rented - cars_moved
             for cars_returned in range(0,capacity):
-                next_state = cars_available + cars_rented + cars_returned
-                value_function[state] += prob_rent_1(cars_rented)*prob_return_1*(reward(cars_rented)+gamma*value_function(next_state))
+                next_state = cars_available - cars_rented + cars_returned
+                value_function[state] += prob_rent_1(cars_rented)*prob_return_1(cars_returned)*(reward(cars_rented)+gamma*value_function[next_state - 1])
         # delta = max(delta, abs(v-state.value)
     print(value_function)
+    plt.plot(value_function)
+    plt.xlabel('Number cars available')
+    plt.ylabel('Value')
+    plt.show()
+
 
 # def learn_policy():
 #     policy_iteration()
+
+
+
+if __name__ == '__main__':
+        policy_evaluation()
